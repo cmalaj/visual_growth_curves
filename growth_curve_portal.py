4a9089095e914aaf9a80c29178c9c4cb1381a45d
Chris Malajczuk <Chris.Malajczuk@telethonkids.org.au>
2025-06-09 13:48:33 +0800

    Added export option.

--- growth_curve_portal.py
+++ growth_curve_portal.py
@@ -14,7 +14,7 @@ import re
 import copy
 
 st.set_page_config(layout="wide")
-st.title("Growth Curve Visualisation Portal YO!")
+st.title("Growth Curve Visualisation Portal (Growth Time-Series + Heatmap)")
 
 
 # Generate 96 distinct colours from the rainbow colormap
