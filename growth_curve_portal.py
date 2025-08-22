41dacadec8886d0083bdb2f8c36d73402aa4c505
Chris Malajczuk <Chris.Malajczuk@telethonkids.org.au>
2025-08-22 13:30:50 +0800

    Fix NameError.

--- growth_curve_portal.py
+++ growth_curve_portal.py
@@ -21,6 +21,9 @@ well_order = [f"{row}{col}" for row in "ABCDEFGH" for col in range(1, 13)]
 well_colours = {well: mcolors.to_hex(rainbow_cmap(i)) for i, well in enumerate(well_order)}
 
 uploaded_files = st.file_uploader("Upload up to 4 LogPhase600 .txt files", type="txt", accept_multiple_files=True)
+all_data = []  # Ensure this exists for conditional rendering later
+
+if uploaded_files:
 
 def time_to_minutes(t):
     h, m, s = map(int, t.split(":"))
